1497fb979bbb7344a971892923ef75fbb1fda393
Aung Myat Moe <am23aao@herts.ac.uk>
2024-06-30 19:44:24 +0100

    update pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@ export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
 cd /home/ubuntu/first-test-app
-git pull origin master
-nvm use v18
+git pull
 npm install &&
-npm build &&
+npm run build &&
 pm2 del 0
 pm2 start run.sh --name first-test-app
